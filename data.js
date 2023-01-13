736ffe408dde956c12da37eeacec7985ba583bba
Raouf Chebri <raouf@chebri.com>
2023-01-13 17:01:07 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,74 @@
+export const data = [
+  {
+    id: 1,
+    brand: 'Nike',
+    model: 'Air Zoom Alphafly',
+    description: "Men's Road Racing Shoes",
+    color: 'Scream Green/Bright Crimson/Honeydew/Black',
+    image:
+      'https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/c24ddc33-6e38-4cc9-b548-dc48cd3528ea/image.jpg',
+  },
+  {
+    id: 2,
+    brand: 'Nike',
+    model: 'ZoomX Vaporfly',
+    description: "Men's Road Racing Shoes",
+    color: 'Scream Green/Bright Crimson/Honeydew/Black',
+    image:
+      'https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/e03f8fa2-652d-4ef8-aa02-1d8b8bb0e018/image.jpg',
+  },
+  {
+    id: 3,
+    brand: 'Nike',
+    model: 'Zoom Fly 5',
+    description: "Men's Running Shoes",
+    color: 'Scream Green/Honeydew/Coconut Milk/Black',
+    image:
+      'https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/065a2e7d-62b9-40fe-948b-3f9eb0dea8b3/image.jpg',
+  },
+  {
+    id: 4,
+    brand: 'Nike',
+    model: 'Air Zoom Pegasus 39',
+    description: "Men's Road Running Shoes",
+    color: 'Scream Green/Coconut Milk/Honeydew/Black',
+    image:
+      'https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/5504e8de-5ea7-4118-90c2-39ee24d52158/image.jpg',
+  },
+  {
+    id: 5,
+    brand: 'Nike',
+    model: 'Pegasus 39',
+    description: "Men's Road Running Shoes",
+    color: 'Anthracite/Black/Lilac/Metallic Pewter',
+    image:
+      'https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/8f3cda35-d270-4508-8d18-bf51377f5768/image.jpg',
+  },
+  {
+    id: 7,
+    brand: 'Nike',
+    model: 'Pegasus 39',
+    description: "Men's Road Running Shoes",
+    color: 'Pure Platinum/Mineral Slate/Bright Spruce/Total Orange',
+    image:
+      'https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/b5d338dd-58b5-4134-8951-692c89477116/image.jpg',
+  },
+  {
+    id: 8,
+    brand: 'Nike',
+    model: 'Pegasus 39',
+    description: "Men's Road Running Shoes",
+    color: 'Black/Anthracite/Vivid Sulfur/Chlorine Blue',
+    image:
+      'https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/ee707e38-a124-4809-a899-f5b46fb1310a/image.jpg',
+  },
+  {
+    id: 9,
+    brand: 'Nike',
+    model: 'Pegasus 39',
+    description: "Men's Road Running Shoes",
+    color: 'Black/Dark Smoke Grey/White',
+    image:
+      'https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/a96f62d4-b20a-4371-a660-38565bc4581b/image.jpg',
+  },
+];
