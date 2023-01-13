edce283ec5f9f75b67c30cb165c065683e9a8c6f
Raouf Chebri <raouf@chebri.com>
2023-01-13 17:33:32 +0100

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,16 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  shoes: any[];
+  message: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const prisma = new PrismaClient();
-  const shoes = await prisma.shoes.findMany();
-  res.status(200).json({ shoes });
+  res.status(200).json({ message: "hi" });
 }
