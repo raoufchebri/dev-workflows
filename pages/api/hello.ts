01c5dd9a96f7cd43ff294a9d087d9a1d6e03010e
Raouf Chebri <raouf@chebri.com>
2023-01-13 15:55:51 +0100

    init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+  shoes: any[];
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const prisma = new PrismaClient();
+  const shoes = await prisma.shoes.findMany();
+  res.status(200).json({ shoes });
 }
